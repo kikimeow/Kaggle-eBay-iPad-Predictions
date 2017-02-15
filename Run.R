010e468c13518cfcf0219b5bdb81d75e667829ca
kikimeow <moominbaby7@hotmail.com>
2017-02-14 16:31:13 -0800

    Update Run.R

--- Run.R
+++ Run.R
@@ -4,14 +4,13 @@
 # **************************************
 
 # set your working directory
-path <- "C:/Users/kikimeow/Documents/Classes/Class- MIT Analytics (R)/Kaggle/Kaggle- ebay"
+path <- "enter path here"
 setwd(path)
 
 # create directory
 dir.create(paste0("data")) # Please put data into this folder
 dir.create(paste0("model output"))
 dir.create(paste0("submission"))
-
 
 # **************************************
 # run scripts
