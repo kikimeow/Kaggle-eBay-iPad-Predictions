52fea82ac02aac313f41dbd709c24c92a4c1b5b1
kikimeow <moominbaby7@hotmail.com>
2017-02-14 16:36:36 -0800

    Update H2Omodel.R

--- H2Omodel.R
+++ H2Omodel.R
@@ -2,32 +2,12 @@
 # Run Ensemble using H2O
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-# Ensemble performance (AUC): 0.905116279069767  "h2o.glm.wrapper"
-# Ensemble performance (AUC): 0.900930232558139  "h2o.deeplearning.wrapper"
-# Ensemble performance (AUC): 0.895116279069767  "h2o.deeplearning.wrapper"
+# Load preprocessed data if not loaded 
+# load("data/preprocessedData.RData")
+fileName <- "submitH2ODeep3"  # This is for naming the file for submission
 
-#RF1: use average price diff 0.85757, 0.86788 (public, private) rfFit.rda
-#RF2: use average price and predict price diff 0.85958, 0.86374 rfFit2.rda
-#RF3: use predict price 0.86220, 0.85943 rfFit3.rda
-#RF4: use average price, predict price, storage price 0.86064, 0.87220  rfFit4.rda
-#RF5: PCA + center, scale word count 0.85584, 0.86892  rfFit5.rda
-#RF6: no PCA, center, scale word count 0.86342, 0.87320 rfFit6.rda
-#RF7: no PCA, center, scale word count (1 word count), H2O to predict price 0.86323, 0.87381 rfFit7.rda
-#H2Odeep: no PCA, center, scale word count 0.85356, 0.88313
-#H2OGBM: no PCA, center, scale word count 0.85468, 0.87999
-#H2Odeep2: no PCA, center, scale word count (word count was counted twice, removed one) 0.85086, 0.87969
-#H2Odeep3: no PCA, center, scale, 1 word count, predict price using H2O 0.85983, 0.88040
-
-
-# Load preprocessed data
-setwd("C:/Users/kikimeow/Documents/Classes/Class- MIT Analytics (R)/Kaggle/Kaggle- ebay") #set working directory
-load("~/Classes/Class- MIT Analytics (R)/Kaggle/Kaggle- ebay/data/preprocessedData.RData")
-fileName <- "submitH2ODeep3"
-
-
-library(h2oEnsemble)  # Requires version >=0.0.4 of h2oEnsemble
-localH2O <- h2o.init(nthreads = -1, max_mem_size = "8G")  # Start an H2O cluster with nthreads = num cores on your machine.  Use all cores available to you (-1)
-h2o.removeAll() # Clean slate - just in case the cluster was already running
+library(h2oEnsemble)  
+localH2O <- h2o.init(nthreads = -1, max_mem_size = "8G") 
 
 # create h2o dataframe for model training
 X_train <- train[split,!(colnames(train)) %in% colExclude]
